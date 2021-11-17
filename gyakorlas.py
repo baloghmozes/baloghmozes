9685ef9babe3aabe0e44ad9ac55721577d791921
balogh899 <92020161+baloghmozes@users.noreply.github.com>
2021-11-17 10:53:46 +0100

    Add files via upload

---
+++ gyakorlas.py
@@ -0,0 +1,5 @@
+#1. feladat: irass ki egymás alá, 8db véletlenszámot (10 és 50 közötti legyen)
+#2. feladat: olvass be egy egész(int) számot, és írassak ki a négyzetét!
+#3. feladat: Kérj be két számot, ami egy téglalap két oldala. Számísd ki a téglalap területét (T) és a kerületét (K)
+#4. feladat: 3. feladat kiegészítése, az oldalhosszak csak pozitív számok lehessenek!
+import random
